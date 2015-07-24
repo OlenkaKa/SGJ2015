152d53863eb88fd383f23797854a10ca9555e226
Aleksandra Karbarczyk <akarbarczyk@gmail.com>
2015-07-24 22:47:25 +0200

    Path finding

---
+++ Assets/Olenka/Scripts/PointFollower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointFollower : MonoBehaviour
+{
+	private Transform player;
+	private NavMeshAgent nav;
+	
+	void Awake ()
+	{
+		nav = GetComponent <NavMeshAgent> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		nav.SetDestination (FindTarget());
+	}
+
+	Vector3 FindTarget()
+	{
+		return player.position;
+	}
+}
