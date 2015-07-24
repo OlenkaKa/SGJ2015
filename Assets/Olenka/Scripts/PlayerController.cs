b968065fafd96dcf680e9185a735966a5a891f23
Aleksandra Karbarczyk <akarbarczyk@gmail.com>
2015-07-24 21:23:13 +0200

    Olenka folder

---
+++ Assets/Olenka/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+		movement = movement.normalized * speed * Time.deltaTime;
+		rb.MovePosition (transform.position + movement);
+	}
+}
