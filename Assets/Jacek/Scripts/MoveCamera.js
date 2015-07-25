c460e4c29b1e1977fa6cc137cbe9308f8a065496
Turkeydipking <jacekmackiewicz96@gmail.com>
2015-07-25 19:40:53 +0200

    Rotacja kamery dziala
    Script na graces i target to rowniez gracz

--- Assets/Jacek/Scripts/MoveCamera.js
+++ Assets/Jacek/Scripts/MoveCamera.js
@@ -1,11 +1,23 @@
 ﻿#pragma strict
 
-var speed : float = 5.0;
- var target : Transform;
+public var speed : float = 0.001;
+var target : Transform;
+public var boundary : float = 5;
+
+private var ScreenWidth : float;
+
+  
+ function Start () {
+ 	ScreenWidth = Screen.width;
+  } 
   
  function Update () {
-     if (Input.GetMouseButton(0)) {
+     if (Input.mousePosition.x > ScreenWidth - boundary) {
          transform.LookAt(target);
-         transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X")*speed);
+         transform.RotateAround(target.position, Vector3.up, 1 *speed);
+     }
+ if (Input.mousePosition.x < 0 + boundary) {
+         transform.LookAt(target);
+         transform.RotateAround(target.position, Vector3.down, 1 *speed);
      }
  }
