0d892a0118e2f70154e25c17635f6e77452e8828
Turkeydipking <jacekmackiewicz96@gmail.com>
2015-07-25 21:47:27 +0200

    praca and mikrofonem prawie skonczona..

--- Assets/Jacek/Scripts/MicrophoneInput.cs
+++ Assets/Jacek/Scripts/MicrophoneInput.cs
@@ -5,7 +5,7 @@ using System;
 public class MicrophoneInput : MonoBehaviour {
 
 
-	//bool audioExists = false;
+	bool audioExists;
 
 	float startTime; // declare this outside any function
 
@@ -13,17 +13,24 @@ public class MicrophoneInput : MonoBehaviour {
 	void Start() {
 		foreach (string device in Microphone.devices) {
 				Debug.Log("Name: " + device);
-			}
+			audioExists = false;
+		}
 	}
 
 	void Update () {
-		if (Input.GetKey("1")){
+		if (Input.GetKey("1") && audioExists == false){
 			// key pressed: save the current time
 			AudioSource aud = GetComponent<AudioSource>();
 			aud.clip = Microphone.Start("null", false, 2, 44100);
 			Debug.Log("recording!!!!");
+			StartCoroutine(AudioCutOff());
 			aud.Play();
+			audioExists = true;
 		}
-	
 	}
+IEnumerator AudioCutOff() {
+		yield return new WaitForSeconds(1.0f);
+		Start ();
+	}
+
 }
