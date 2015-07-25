bc56a00e0035427a38a1fce3d5ab1d75866d8c8d
Aleksandra Karbarczyk <akarbarczyk@gmail.com>
2015-07-25 20:52:34 +0200

    Wstępna wersja policji

---
+++ Assets/Scripts/PoliceGroup.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoliceGroup : MonoBehaviour {
+
+	public int policeAmount;
+	public Object policePrefab;
+
+	private List<Policeman> police;
+	
+	void Start ()
+	{
+		police = new List<Policeman> ();
+		for (int i = 0; i < policeAmount; ++i)
+		{
+			Vector3 startPos = transform.position + new Vector3 (Random.Range(1f, 5f), 0.5f, Random.Range(1f, 5f));
+			GameObject policemanObj = (GameObject)Instantiate (policePrefab, startPos, Quaternion.identity);
+
+			Policeman policeman = policemanObj.GetComponent<Policeman>();
+			policeman.group = this;
+			policeman.state = Policeman.PoliceState.Waiting;
+			policeman.home = startPos;
+			policeman.maxDistanceFromHome = 10;
+			policeman.distanceToTarget = 5;
+			policeman.rayRange = 5;
+
+			police.Add(policeman);
+		}
+		Debug.Log (police.Count);
+	}
+
+	public void RemovePoliceman(Policeman p)
+	{
+		police.Remove (p);
+	}
+
+	public void BroadcastTarget(Transform target)
+	{
+		for (int i = 0; i < police.Count; ++i)
+		{
+			if(police[i].state != Policeman.PoliceState.Following)
+				police[i].SetTarget(target);
+		}
+	}
+}
---
+++ Assets/Scripts/Policeman.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Policeman : MonoBehaviour
+{
+	public enum PoliceState
+	{
+		Waiting, Following, Returning
+	}
+
+	public PoliceGroup group;
+	public Vector3 home;
+
+	public Transform target;
+	public PoliceState state;
+
+	public float distanceToTarget; // after this distance police attack
+	public float rayRange;
+	public float maxDistanceFromHome;
+
+	private NavMeshAgent nav;
+	
+	void Start ()
+	{
+		state = PoliceState.Waiting;
+		nav = GetComponent <NavMeshAgent> ();
+
+		StartCoroutine (UpdateTarget ());
+	}
+
+	IEnumerator UpdateTarget ()
+	{
+		while (true)
+		{
+			if (state == PoliceState.Following)
+			{
+				if(!InPatrolArea())
+					state = PoliceState.Returning;
+				else if(Vector3.Distance (target.position, transform.position) > distanceToTarget)
+					nav.SetDestination (target.position);// + offset);
+				//else
+				//	;//attack
+			}
+			
+			else if (state == PoliceState.Returning)
+			{
+				if(transform.position == home)
+					state = PoliceState.Waiting;
+				else
+					nav.SetDestination (home);
+			}
+			
+			else
+			{
+				nav.destination = transform.position;
+			}
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	bool ObstacleDetection ()
+	{
+		Vector3 direction = nav.destination - transform.position;
+		Ray ray = new Ray (transform.position, direction.normalized);
+		RaycastHit hit;
+		
+		if(Physics.Raycast (ray, out hit, rayRange))
+		{
+			if(hit.collider.gameObject.tag == "Obstacle")
+			{
+				Debug.Log ("Obstacle detected");
+				return true;
+			}
+			
+		}
+		return false;
+	}
+	
+	void Death ()
+	{
+		group.RemovePoliceman (this);
+		Destroy (gameObject);
+	}
+
+	bool InPatrolArea()
+	{
+		if (Vector3.Distance (target.position, transform.position) > maxDistanceFromHome)
+			return false;
+		return true;
+	}
+
+	public void SetTarget(Transform t)
+	{
+		target = t;
+		state = PoliceState.Following;
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.tag == "Civilian" || other.tag == "Player")
+		{
+			SetTarget(other.transform);
+			group.BroadcastTarget (target);
+		}
+	}
+}
